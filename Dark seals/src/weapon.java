7da445da20f490cf6047b62aad4a10c3ccc8869b
Seal <joakim.kattelus@gmail.com>
2015-12-27 18:10:24 +0200

    First.
    Signed-off-by: Seal <joakim.kattelus@gmail.com>

---
+++ Dark seals/src/weapon.java
@@ -0,0 +1,77 @@
+import processing.core.*;
+
+public class weapon extends PApplet {
+
+	float reach=100;
+	int e;  
+	float rotX = 0;  
+	  
+
+	float sX;
+	float sY;
+	
+	void melee (actor attacker) {
+		
+
+		  if (attacker.swordL == true) {
+		    if(rotX>=0 && rotX<radians(135)){
+		      rotX=radians(135);}
+		  sX = reach * cos( rotX ) + attacker.location.x;
+		  sY = reach * sin( rotX) + attacker.location.y;  
+		   
+		    rotX=rotX+radians(4);
+		    
+		        if(rotX>=radians(225)){
+		      rotX=radians(0);
+		      attacker.swordL = false;}
+		    
+		  }
+		  
+		    if (attacker.swordU == true) {
+		    if(rotX>=0 && rotX<radians(225)){
+		      rotX=radians(225);}
+		  sX = reach * cos( rotX ) + attacker.location.x;
+		  sY = reach * sin( rotX) + attacker.location.y;  
+		   
+		    rotX=rotX+radians(4);
+		    
+		        if(rotX>=radians(315)){
+		      rotX=radians(0);
+		      attacker.swordU = false;}
+		    
+		  }
+		  
+		    if (attacker.swordR == true) {
+		    if(rotX>=0 && rotX<radians(315)){
+		      rotX=radians(315);}
+		  sX = reach * cos( rotX ) + attacker.location.x;
+		  sY = reach * sin( rotX) + attacker.location.y;  
+		   
+		    rotX=rotX+radians(4);
+		    
+		        if(rotX>=radians(405)){
+		      rotX=radians(0);
+		      attacker.swordR = false;}
+		    
+		  }
+		      if (attacker.swordD == true) {
+		    if(rotX>=0 && rotX<radians(405)){
+		      rotX=radians(405);}
+		  sX = reach * cos( rotX ) + attacker.location.x;
+		  sY = reach * sin( rotX) + attacker.location.y;  
+		   
+		    rotX=rotX+radians(4);
+		    
+		        if(rotX>=radians(495)){
+		      rotX=radians(0);
+		      attacker.swordD = false;}
+		    
+		  }
+
+		  
+		  float wa = atan2(sY-attacker.location.y, sX-attacker.location.x);
+	
+	
+	
+	}
+}
