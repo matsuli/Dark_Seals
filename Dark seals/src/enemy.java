8dbb45a6cf8b28faa9c85afb5d0bc75b39b2c33b
seal9 <joakim.kattelus@gmail.com>
2015-12-29 20:16:39 +0200

    pls work now

--- Dark seals/src/enemy.java
+++ Dark seals/src/enemy.java
@@ -2,5 +2,14 @@ import processing.core.*;
 
 public class enemy extends actor {
 
-
+	  PVector elocationTest;
+	  float eoldPosX, eoldPosY, erotation2, espeed;
+	  float EnemyRadius = 20;
+	  boolean toRemove2;
+	  int enemyState=1;
+	  float sightRadius=180;
+	  boolean nearCenter;
+	  float noDetectTimer = 0;
+	  float ad=0;
+	  
 }
