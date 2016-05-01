9a3fbcc59f2920761111b71b8e5a2f4df510c5b0
MexicanBull <balder.hernandez@gmail.com>
2016-05-01 20:50:19 +0300

    ny commit

--- SimpleGameEngine/src/Bullet.java
+++ SimpleGameEngine/src/Bullet.java
@@ -1,4 +1,18 @@
+import java.awt.Point;
 
 public class Bullet {
-
+	Actor shooter;
+	boolean toRemove=false;
+	float radius = 5;
+	float oldPosX, oldPosY, rotation, speed;
+	Point location = new Point ();//bullet location
+	
+	Bullet (actor shooter, float targetPosX, float targetPosY) {
+		this.shooter=shooter;
+	    location= new PVector(shooter.location.x, shooter.location.y);
+	    oldPosX = targetPosX;
+	    oldPosY = targetPosY;
+	    rotation = atan2(oldPosY - location.y, oldPosX - location.x) / PI * 180;
+	    speed = 15;
+	  }
 }
