c8d8f12a27378e78ec02ba0353a25f23904b284a
MexicanBull <balder.hernandez@gmail.com>
2016-05-01 21:10:41 +0300

    bullets

--- SimpleGameEngine/src/Bullet.java
+++ SimpleGameEngine/src/Bullet.java
@@ -4,15 +4,28 @@ public class Bullet {
 	Actor shooter;
 	boolean toRemove=false;
 	float radius = 5;
-	float oldPosX, oldPosY, rotation, speed;
-	Point location = new Point ();//bullet location
+	float oldPosX, oldPosY, speed;
+	double rotation;
+	Point location;//bullet location
 	
-	Bullet (actor shooter, float targetPosX, float targetPosY) {
+	Bullet (Actor shooter, float targetPosX, float targetPosY) {
 		this.shooter=shooter;
-	    location= new PVector(shooter.location.x, shooter.location.y);
+	    location = new Point (shooter.location.x, shooter.location.y);
 	    oldPosX = targetPosX;
 	    oldPosY = targetPosY;
-	    rotation = atan2(oldPosY - location.y, oldPosX - location.x) / PI * 180;
+	    rotation = Math.atan2(oldPosY - location.y, oldPosX - location.x) / Math.PI * 180;
 	    speed = 15;
+	  }
+	
+	void update() {
+	    location.x = (int) (location.x + Math.cos(rotation/180*Math.PI)*speed);
+	    location.y = (int) (location.y + Math.sin(rotation/180*Math.PI)*speed);
+
+
+	    if (location.x > 0 && location.x < SimpleGameEngine.windowWidth && location.y > 0 && location.y < SimpleGameEngine.windowHeight && toRemove==false) {
+	    } else {
+	      SimpleGameEngine.bullets.remove(this);		//removar denna bullet
+	    }
+	   
 	  }
 }
