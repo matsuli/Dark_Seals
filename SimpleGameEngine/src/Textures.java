c88b233c519ddf23459d86eb861eba281e215fcb
MexicanBull <balder.hernandez@gmail.com>
2016-06-08 17:51:55 +0300

    gjaop

--- SimpleGameEngine/src/Textures.java
+++ SimpleGameEngine/src/Textures.java
@@ -1,8 +1,17 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Textures {
 	
 	public void loadTexture () {
-		
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("images/chicken.gif"));
+		} catch (IOException e) {
+		}
 	}
 	
 }
