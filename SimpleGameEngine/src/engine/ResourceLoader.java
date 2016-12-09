93a7c8c29baec135d928e6f05ad35d7636a96d3d
MexicanBull <balder.hernandez@gmail.com>
2016-12-09 18:49:35 +0200

    bättre sätt att lagra resurser
    med den här classen sparar vi varje fil en gång istället för att ladda
    ett skilt exemplar varje gång vi vill ha t.ex. en bild (alltså istället
    för att ladda samma bild varje gång vi vill ha den, så laddas den en
    gång och accessas flera)

---
+++ SimpleGameEngine/src/engine/ResourceLoader.java
@@ -0,0 +1,33 @@
+package engine;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ResourceLoader {
+	
+	Map <String, BufferedImage> resources = new HashMap<String, BufferedImage>();
+	
+	public ResourceLoader (){
+		
+	}
+	void addResource (String name, String type, String source){
+		if (type=="image"){
+			BufferedImage img;
+			try{
+				img = ImageIO.read(new File(source));
+				resources.put(name,img);
+			} catch (IOException e) {
+				System.out.println("Failed to load image");
+			}
+		}
+	}
+	
+}
